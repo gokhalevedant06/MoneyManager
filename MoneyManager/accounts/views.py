5786fa9f893303b1cbfbff464fd605f8ecad5ae7
mitrajeet <mitrajeetgolsangi@gmail.com>
2021-03-05 09:00:58 +0530

    Added login logout utility and linked project to local database also added fontawesome icon library

--- MoneyManager/accounts/views.py
+++ MoneyManager/accounts/views.py
@@ -1,11 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import auth, User
 
 # Create your views here.
 
 
 def login(request):
-    return render(request, 'login.html')
+    ''' Logs in  a user an authenticates him '''
+
+    if request.method == 'POST':
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+
+        user = auth.authenticate(username=username, password=password)
+        if user:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
+
+
+def logout(request):
+    ''' Logs out a user '''
+
+    auth.logout(request)
+    return redirect('/')
 
 
 def register(request):
+    ''' Registers a new user '''
+
     return render(request, 'register.html')
