ab874b579865adebd959ecfbb7ff195457068ae3
Vedant Gokhale <gokhalevedant06@gmail.com>
2021-03-06 13:16:05 +0530

    Updated Login page,Home page

--- MoneyManager/renderer/urls.py
+++ MoneyManager/renderer/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
-    path('login/', views.login)
+    path('login/', views.login),
+    path('new/',views.new),
+    path('user_info/', views.user_info)
 ]
