a96a2fdabc17f72c0e53d1bffbf3f40c808516dc
mitrajeet <mitrajeetgolsangi@gmail.com>
2021-03-06 09:34:48 +0530

    Adding registration form and basic login utility to project

--- MoneyManager/MoneyManager/settings.py
+++ MoneyManager/MoneyManager/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import mimetypes
 from pathlib import Path
 import os
 
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'crispy_forms',
+    'livereload',
 
     'accounts',
     'home',
@@ -125,8 +128,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+
+mimetypes.add_type('text/css', '.css', True)
+
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
