65d888455ff40e589ea7049e86e2073d574ca88a
mitrajeet <mitrajeetgolsangi@gmail.com>
2021-03-08 20:02:52 +0530

    adding about us template to the project

--- MoneyManager/accounts/urls.py
+++ MoneyManager/accounts/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('login/', views.login, name="login"),
     path('register/', views.register, name="register"),
     path('logout/', views.logout, name="logout"),
+
+    path('user-info', views.user_info, name="user_info"),
+    path('user-info-ajax', views.user_info_ajax, name="user_info_ajax"),
 ]
--- MoneyManager/home/views.py
+++ MoneyManager/home/views.py
@@ -1,12 +1,4 @@
 from django.shortcuts import render
-from .models import *
-
-from pathlib import Path
-
-from django.core.serializers import serialize
-from django.core.serializers.json import DjangoJSONEncoder
-
-from django.http import JsonResponse
 # Create your views here.
 
 
@@ -14,16 +6,5 @@ def index(request):
     return render(request, 'index.html')
 
 
-def user_info(request):
-    if request.method == 'POST':
-        pass
-    else:
-        if request.is_ajax():
-            choices = Choice.objects.all()
-
-            serialized_choices = serialize(
-                'json', choices, cls=DjangoJSONEncoder)
-
-            return JsonResponse(serialized_choices, safe=False)
-
-        return render(request, "user_info.html")
+def about(request):
+    return render(request, 'about.html')
