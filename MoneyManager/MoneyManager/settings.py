169b5364f48fc6f2f68abdb6b4bb8e7a7cbe044e
mitrajeet <mitrajeetgolsangi@gmail.com>
2021-04-12 22:01:04 +0530

    debuggin 11

--- MoneyManager/MoneyManager/settings.py
+++ MoneyManager/MoneyManager/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'accounts',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -121,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIR = [
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
