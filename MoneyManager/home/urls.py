176303e70f038778ab428925c8f76c3e48815030
mitrajeet <mitrajeetgolsangi@gmail.com>
2021-04-12 22:07:16 +0530

    debugging 16

--- MoneyManager/home/urls.py
+++ MoneyManager/home/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('about-us/', views.about, name="about"),
     path('sensex-graph/', views.sensex_graph, name="sensex_graph"),
+    path('nifty-graph/', views.nifty_graph, name="nifty_graph"),
 ]
