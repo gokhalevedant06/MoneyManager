fcb4e46e8dfdc44e8729dee033ee63f3ab7a9f8d
mitrajeet <mitrajeetgolsangi@gmail.com>
2021-04-14 19:21:31 +0530

    origin error solving 4

--- MoneyManager/home/admin.py
+++ MoneyManager/home/admin.py
@@ -1,18 +1,3 @@
 from django.contrib import admin
-from . models import *
 
 # Register your models here.
-
-
-@admin.register(Choice)
-class ChoiceAdmin(admin.ModelAdmin):
-    list_display = [
-        'field',
-        'value'
-    ]
-    list_display_links = [
-        'value'
-    ]
-
-
-admin.site.register(UserInfo)
--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
+from .models import *
 # Register your models here.
+
+
+@admin.register(Choice)
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = [
+        'field',
+        'value'
+    ]
+    list_display_links = [
+        'value'
+    ]
+
+
+admin.site.register(UserInfo)
