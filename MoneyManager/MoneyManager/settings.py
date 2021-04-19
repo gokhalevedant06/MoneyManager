9f6b6f32ad9a8c1a9e1d1426811bb6ab7e1debb1
mitrajeet <mitrajeetgolsangi@gmail.com>
2021-04-19 22:11:20 +0530

    data change functionality debugging 3

--- MoneyManager/MoneyManager/settings.py
+++ MoneyManager/MoneyManager/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'MoneyManager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'money_manager',
+        'HOST': 'localhost',
+        'USER': 'postgres',
+        'PASSWORD': os.environ.get('PSQL_PAS'),
+        'PORT': '5432',
     }
 }
 
