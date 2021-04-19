78fd99afe24c35c440cc1b89f145122657d60dff
mitrajeet <mitrajeetgolsangi@gmail.com>
2021-04-19 22:12:46 +0530

    data change functionality debugging 12

--- MoneyManager/expenses/urls.py
+++ MoneyManager/expenses/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="exp_index"),
-    path('fill-out-expenses', views.form, name="exp_form"),
+    path('fill-out-expenses/', views.form, name="exp_form"),
+    path('expenses-form-ajax/', views.form_ajax, name="exp_form_ajax"),
 ]
