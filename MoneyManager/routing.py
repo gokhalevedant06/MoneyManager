b3d00af947ff57225f6bbc58d00ce2a8a7f421e6
Mitrajeet Golsangi <mitrajeetgolsangi@gmail.com>
2021-07-16 15:12:17 +0530

    ASGDeploy 02

--- MoneyManager/routing.py
+++ MoneyManager/routing.py
@@ -1,5 +1,8 @@
-from channels.routing import ProtocolTypeRouter
+import os
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.urls import path
 
 application = ProtocolTypeRouter({
-    
 })
